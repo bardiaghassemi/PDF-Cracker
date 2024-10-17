3e909cf71187d7ac99a1f64d721814ce2bd2e65b
Hack3r <129080315+bardiaghassemi@users.noreply.github.com>
2024-10-17 13:00:33 -0400

    Update Cracker.py

--- Cracker.py
+++ Cracker.py
@@ -137,6 +137,8 @@ def main():
 
                 for password in passlistall:
                     password = password.strip("\n")
+                    if password == '' or password == '\n' or password == None:
+                        continue
                     try:
                         pikepdf.open(pdf_file, password=password)
                         print(Fore.GREEN, f"\b\n[+] Password : {password}", Fore.RESET)
