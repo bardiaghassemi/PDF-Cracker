68f6dc55323a56aa4f6483077f560ad41f5292e4
H3ck3r <129080315+bardiaghassemi@users.noreply.github.com>
2024-12-04 18:20:04 +0330

    Update Cracker.py

--- Cracker.py
+++ Cracker.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 
         p = argparse.ArgumentParser()
         
-        p.description = "(.❛ ᴗ ❛.) PDF-Cracker Is A Tool For Break The Lock Of PDF Files And Find Passwords (.❛ ᴗ ❛.)"
+        p.description = "(.❛ ᴗ ❛.) PY-PDF-Cracker Is A Tool For Break The Lock Of PDF Files And Find Passwords (.❛ ᴗ ❛.)"
 
         p.add_argument("-f", dest="PDF_File", help="PDF File For Start Cracking", required=True)
         p.add_argument("-p", dest="PASSWORD_FILE", help="Password File For Use it in Cracking, If You Has No Password File Not Use This Option")
